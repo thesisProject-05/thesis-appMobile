eecf0f89862a882a1507857f6137113bc261f23e
aymensouissi <aymensouissi@gmail.com>
2022-11-15 23:17:01 +0100

    installed react-native-select-dropdown and react-native-modal-dropdown and finished ownerview and finished footer routes

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import OnerView from './assets/screens/OunerView/OnerView'
+import  Chat from './assets/screens/Chat'
+import Home from "./assets/screens/Home";
+import Blog from "./assets/screens/Blog";
+import Login  from "./assets/screens/Login"
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>RoostIt</Text>
-    </View>
-  );
-}
+
+
 export default function App() {
   const Stack = createNativeStackNavigator();
 
@@ -18,9 +18,21 @@ export default function App() {
   return (
     <NavigationContainer style={styles.container}>
     <Stack.Navigator>
-    <Stack.Screen name="home" component={HomeScreen} />
+   
+
+    <Stack.Screen name="owner" component={OnerView} />
+    <Stack.Screen name="chat" component={Chat} />
+    <Stack.Screen name="home" component={Home} />
+    <Stack.Screen name="blog" component={Blog} />
+    <Stack.Screen name="login" component={Login} />
+
+    
+    
+    
+   
       </Stack.Navigator>
     </NavigationContainer>
+  
   );
 }
 
